b9051a5ecf2be51b4854260d1daff0a7002258d7
olaniyiOlawale <jao.olaniyi@gmail.com>
2022-03-17 14:28:35 +0100

    assignment

---
+++ main.js
@@ -0,0 +1,13 @@
+alert ("Welcome to my JScalculation")
+
+let b1 = prompt("total bill");
+let f1 = prompt("number of friend");
+let p1 = prompt("percentage");
+
+num.indexHTML= b1;
+frnd.indexHTML= f1;
+pct.indexHTML= p1;
+
+const data = {
+
+}
