8729a025c6ab8be2082efca20c65632111a5de25
olaniyiOlawale <jao.olaniyi@gmail.com>
2022-03-17 16:09:18 +0100

    cw1

--- main.js
+++ main.js
@@ -4,10 +4,28 @@ let b1 = prompt("total bill");
 let f1 = prompt("number of friend");
 let p1 = prompt("percentage");
 
-num.indexHTML= b1;
-frnd.indexHTML= f1;
-pct.indexHTML= p1;
+ let bill = parseInt(b1);
+ let friend = parseInt(f1);
+ let percent = parseInt(p1);
 
-const data = {
+ let totalpay = bill + (percent/100 * bill)
+ let share = totalpay/friend
 
-}
+ console.log(totalpay);
+ console.log(share);
+
+num.innerHTML= b1;
+frnd.innerHTML= f1;
+pct.innerHTML= p1;
+
+
+
+// const data = {
+//     "total" : {
+//         "bill": b1,
+//         "friend": f1,
+//          "percentage": p1,
+//     }
+
+// }
+// console.log(data);
